21c115583451672d07c567d54c67d5df421ca731
SadakMefteh <144259360+SadakMefteh@users.noreply.github.com>
2025-03-07 00:35:09 +0100

    all crud terminé

--- src/app/pages/tables/clients/clients.component.ts
+++ src/app/pages/tables/clients/clients.component.ts
@@ -16,12 +16,12 @@ export class ClientsComponent implements OnInit {
   public isAddMode = false;
   public isEditMode = false;
   public formData: User = {
-    fullname: '',
-    email: '',
-    phone: '',
-    password: '', // Ajouter le champ password
-    role: ''
-  };
+  fullname: '',
+  email: '',
+  phone: '',
+  password: '',
+  role: 'client' // Valeur par défaut
+};
   public currentPage: number = 1;
   public itemsPerPage: number = 5;
   public searchText: string = '';
@@ -30,7 +30,7 @@ export class ClientsComponent implements OnInit {
 
   ngOnInit() {
     this.tableData1 = {
-      headerRow: ['ID', 'Full Name', 'Phone', 'Email', 'Role'], // En-têtes mis à jour
+      headerRow: ['ID', 'Full Name', 'Email', 'Phone', 'Role'], // En-têtes mis à jour
       dataRows: []
     };
     this.getUsers(); // Fetch all clients when component loads
@@ -99,10 +99,17 @@ export class ClientsComponent implements OnInit {
   }
 
   updateRow() {
+    const user = this.paginatedRows.find(u => u.id === this.formData.id);
+  
+    // Si le champ password est vide, conservez le mot de passe existant
+    if (!this.formData.password) {
+      this.formData.password = user?.password || '';
+    }
+  
     this.usersService.updateUser(this.formData.id!, this.formData).subscribe(
       (updatedUser) => {
         console.log('User updated:', updatedUser);
-        this.getUsers(); // Mettre à jour la liste des clients après l'édition
+        this.getUsers(); // Rafraîchir la liste des utilisateurs
         this.cancelForm();
       },
       (error) => {
